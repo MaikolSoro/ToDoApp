2d2680f40a387570bb2fc1ba53d72ccc16caa369
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-12-17 19:22:19 -0600

    Display Data in RecyclerView

--- app/src/main/java/com/michael/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/michael/todoapp/fragments/list/ListFragment.kt
@@ -3,11 +3,18 @@ package com.michael.todoapp.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.michael.todoapp.R
+import com.michael.todoapp.data.viewmodel.ToDoViewModel
 
 
 class ListFragment : Fragment() {
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private  val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +23,11 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
 
+        //val recyclerView = view.recyclerView recyclerView.adapter = adapter
+         //       recylerView.layoutManager = LinearLayoutManager(requireActivity())
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer {
+            data-> adapter.setData(data)
+        })
 //        view.floatingActionButton.setOnClickListener {
 //            findNavController().navigate(R.id.action_listFragment_to_addFragment)
 //        }

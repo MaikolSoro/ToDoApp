10a31dd99772a83e86283f29b1d467f540625e36
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-12-17 18:11:58 -0600

    - Insert Data to DataBase - Created Shared View Model

---
+++ app/src/main/java/com/michael/todoapp/fragments/SharedViewModel.kt
@@ -0,0 +1,30 @@
+package com.michael.todoapp.fragments
+
+import android.app.Application
+import android.text.TextUtils
+import androidx.lifecycle.AndroidViewModel
+import com.michael.todoapp.data.models.Priority
+
+class SharedViewModel(application: Application): AndroidViewModel(application) {
+
+     fun verifyDataFromUser(title: String, description: String): Boolean {
+        return if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
+            false
+        } else !(title.isEmpty() || description.isEmpty())
+    }
+
+     fun parsePriority(priority: String): Priority {
+        return when (priority) {
+            "High Priority" -> {
+                Priority.HIGH
+            }
+            "Medium Priority" -> {
+                Priority.MEDIUM
+            }
+            "Low Priority" -> {
+                Priority.LOW
+            }
+            else -> Priority.LOW
+        }
+    }
+}
--- app/src/main/java/com/michael/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/michael/todoapp/fragments/add/AddFragment.kt
@@ -2,22 +2,73 @@ package com.michael.todoapp.fragments.add
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.michael.todoapp.R
-
+import com.michael.todoapp.data.models.ToDoData
+import com.michael.todoapp.data.viewmodel.ToDoViewModel
+import com.michael.todoapp.fragments.SharedViewModel
+import com.michael.todoapp.databinding.FragmentAddBinding
 class AddFragment : Fragment() {
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
+
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        // Set Menu
+        setHasOptionsMenu(true)
+        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.add_fragment_menu,menu)
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.menu_add) {
+            insertDataToDb()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDb() {
+        val mTitle = binding.titleEt.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEt.text.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
+
+        if (validation) {
+            // Insert Data to Database
+
+            val newData = ToDoData(
+                0,
+                mTitle,
+                mSharedViewModel.parsePriority(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
+
+            // Navigate back
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
